128d4cc044159102e9601ca37175ba89604c42c6
kushaniM <sitharirajapaksha@gmail.com>
2017-11-29 13:47:05 +0530

    first commit

---
+++ IT14143632/spdclab07/spdclab07/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+
+namespace spdclab07
+{
+    public partial class Form1 : Form
+    {
+
+        MessageQueue mesageq = new MessageQueue();
+
+        public Form1()
+        {
+            InitializeComponent();
+            mesageq.Path = @".\private$\Bills2";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(MessageQueue.Exists(mesageq.Path))
+            {
+                //Exists
+                sendData2Queue();
+            }
+            else
+            {
+                // Creates the new queue named "Bills"
+                MessageQueue.Create(mesageq.Path);
+                sendData2Queue();
+            }
+        }
+
+        private void sendData2Queue()
+        {
+            mesageq.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            mesageq.ReceiveCompleted += billingQ_ReceiveCompleted;
+            mesageq.Send(DateTime.Now.ToString());
+            mesageq.Send("Hureaaa");
+            mesageq.Send(DateTime.Now.ToString());
+            // mesageq.Send("Hujghghghggreaaa");
+            mesageq.BeginReceive();
+            mesageq.Close();
+
+        }
+
+        void billingQ_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            try
+            {
+                var msg = mesageq.EndReceive(e.AsyncResult);
+                string data = msg.Body.ToString();
+                MessageBox.Show(DateTime.Now.ToString());
+                // Process the logic be sending the message
+                //Restart the asynchronous receive operation.
+                mesageq.BeginReceive();
+            }
+            catch (MessageQueueException qexception){
+               // MessageBox.Show("kfgjhbjkgh;fjkd;jkdfgh;fjkdgh;fjkdgh");
+            }
+
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+ID No: IT14143632
+Name : Kushani Kariyapperuma
+Batch : 3rd year 2nd semester weekend 
